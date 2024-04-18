2843de254ff29cc465ccb56cce48a82ed3b9eeb2
kadzielas <sebastian.kadziela@outlook.com>
2024-04-18 11:15:58 +0200

    #4 Update

---
+++ src/main/java/carDealer/BuyCar.java
@@ -0,0 +1,2 @@
+package carDealer;public class BuyCar {
+}
--- src/main/java/carDealer/Cars.java
+++ src/main/java/carDealer/Cars.java
@@ -3,12 +3,13 @@ package carDealer;
 import java.math.BigDecimal;
 import java.util.Scanner;
 
-public class ListOfReadyCars extends ListOfCars{
+public class BuyCar extends ListOfCars{
     public static void CarList(){
         Scanner scanner = new Scanner(System.in);
         int menu;
 
         CreateCar test1 = new CreateCar();
+        test1.setID(12);
         test1.setManufacturer("Audi");
         test1.setModel("a7");
         test1.setPrice2(new BigDecimal("96000.00"));
@@ -17,6 +18,7 @@ public class ListOfReadyCars extends ListOfCars{
         test1.addCar(test1);
 
         CreateCar test2 = new CreateCar();
+        test2.setID(5);
         test2.setManufacturer("Seat");
         test2.setModel("Ibiza");
         test2.setPrice2(new BigDecimal("8000.00"));
@@ -25,6 +27,7 @@ public class ListOfReadyCars extends ListOfCars{
         test2.addCar(test2);
 
         CreateCar test3 = new CreateCar();
+        test3.setID(16);
         test3.setManufacturer("Skoda");
         test3.setModel("Fabia");
         test3.setPrice2(new BigDecimal("7000.00"));
@@ -33,9 +36,9 @@ public class ListOfReadyCars extends ListOfCars{
         test3.addCar(test3);
 
         do {
-            System.out.println("1.Every available car");
-            System.out.println("2.Filter by manufacturer");
-            System.out.println("3.Soon");
+            System.out.println("1.Show list of available cars");
+            System.out.println("2.Find cars by manufacturer");
+            System.out.println("3.Check price on loan");
             System.out.println("4.Home page\n");
             System.out.print("Where are we going?: ");
             menu = scanner.nextByte();
